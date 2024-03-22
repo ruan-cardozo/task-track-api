8e97ca41a4fee20208a50595df0967dea9375078
ruan-cardozo <ruanvcardozo@gmail.com>
2024-03-22 00:36:13 -0300

    setuping project do create api to task track

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  apiTest(): string {
+    return '<h1> API do TaskTrack </h1>';
   }
 }
