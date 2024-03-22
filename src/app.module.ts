bd66d1d87bdcb6ab2c34cd74f83597bdaff17b25
ruan-cardozo <ruanvcardozo@gmail.com>
2024-03-22 11:54:37 -0300

    spreading db compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(), 
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'task_db',
+      port: 5432,
+      database: 'postgres',
+      username: 'postgres',
+      password: 'postgres',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
